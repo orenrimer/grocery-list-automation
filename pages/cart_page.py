9926c020a9cd910b8d5d43777a9daf93604032ee
Oren Rimer <76216721+orenrimer@users.noreply.github.com>
2020-12-29 12:12:07 +0200

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -15,4 +15,14 @@ class CartPage(BasePage):
     def clear_cart(self):
         items = self.driver.find_elements_by_xpath(self.REMOVE_BTN)
         for item in items:
-            item.click()
+            
+            max_tries = 3
+            try_num = 0
+            while try_num < max_tries:
+                try:
+                    item.click()
+                    break
+                except Exception as e:
+                    try_num += 1
+            else:
+                raise Exception(f"failed to click on elements:: {locator} after {max_tries} tries")
